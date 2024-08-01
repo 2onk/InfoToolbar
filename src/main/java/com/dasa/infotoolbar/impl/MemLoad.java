c13d99cbcaa0dfe08baa1c3182abd25c93ef0f7b
2onk <122785824+2onk@users.noreply.github.com>
2024-08-01 14:29:40 +0200

    Update MemLoad.java
    changed to more accurate memAvailable

--- src/main/java/com/dasa/infotoolbar/impl/MemLoad.java
+++ src/main/java/com/dasa/infotoolbar/impl/MemLoad.java
@@ -17,22 +17,22 @@ public class MemLoad {
             memReader = new BufferedReader(new InputStreamReader(new FileInputStream("/proc/meminfo")));
             String line = "";
             long memtotal = 0L;
-            long memfree = 0L;
+            long memavailable = 0L;
             while ((line = memReader.readLine()) != null) {
                 final String[] mem_cols = line.split("\\s+");
                 if (mem_cols[0].equals("MemTotal:")) {
                     memtotal = Long.parseLong(mem_cols[1]);
                 }
-                else if (mem_cols[0].equals("MemFree:")) {
-                    memfree = Long.parseLong(mem_cols[1]);
+                else if (mem_cols[0].equals("MemAvailable:")) {
+                	memavailable = Long.parseLong(mem_cols[1]);
                 }
-                if (memtotal != 0L && memfree != 0L) {
+                if (memtotal != 0L && memavailable != 0L) {
                     break;
                 }
             }
-            final float usage_percent = 100.0f * (1.0f - memfree / (float)memtotal);
-            System.out.println(String.format(Locale.ENGLISH, "total: %d, free: %d, percent: %05.2f, mb-used: %d", memtotal, memfree, usage_percent, (memtotal - memfree) / 1024L));
-            usage_msg = String.valueOf(String.format(Locale.ENGLISH, "%05.2f", usage_percent)) + "% - " + String.format(Locale.ENGLISH, "%d MB used", (memtotal - memfree) / 1024L);
+            final float usage_percent = 100.0f * (1.0f - memavailable / (float)memtotal);
+            //System.out.println(String.format(Locale.ENGLISH, "total: %d, free: %d, percent: %05.2f, mb-used: %d", memtotal, memavailable, usage_percent, (memtotal - memavailable) / 1024L));
+            usage_msg = String.valueOf(String.format(Locale.ENGLISH," %d MB, available: %d MB, used: %05.2f %% , mb-used: %d", (memtotal/1024L), (memavailable/1024L), usage_percent, (memtotal - memavailable) / 1024L));
         }
         catch (Exception e) {
             System.out.println("Exception '" + e.getMessage() + "'occured with trace: " + e.getStackTrace());
